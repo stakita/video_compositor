497701b144b6101b10cf1831aa3b4a6a117c0dd4
Simon Takita <smtakita@gmail.com>
2022-07-09 06:36:29 -0700

    Replace gen_wave tools

--- create_waveform_video.py
+++ create_waveform_video.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python3
-'''gen_wave_plot.py
-Generate waveform plot of audio channels in input file (video/audio).
+'''create_waveform_video.py
+Generate audio waveform progress video.
 
 Usage:
-  gen_wave_plot.py <input_media_file> [--output=<OUTPUT_FILE>] [--width=<WIDTH>] [--height=<HEIGHT>] [--channels=<CHANNELS>]
-  gen_wave_plot.py (-h | --help)
+  create_waveform_video.py <input_media_file> <total_seconds> [--output=<OUTPUT_FILE>] [--width=<WIDTH>] [--height=<HEIGHT>] [--channels=<CHANNELS>] [--fps=<FPS>]
+  create_waveform_video.py (-h | --help)
 
 Options:
   -h --help                 Show this screen.
-  --output=<OUTPUT_FILE>    Output file name [default: waveplot.png]
+  --output=<OUTPUT_FILE>    Output file name [default: waveform.mp4]
   --width=<WIDTH>           Width of the image [default: 2262]
   --height=<HEIGHT>         Width of the image [default: 200]
   --channels=<CHANNELS>     Set mono(1) or stereo(2) [default: 1]
+  --fps=<FPS>               Override frames per second [default: 24]
 '''
 import sys
+import copy
 
 try:
+    from cv2 import cv2
     from docopt import docopt
+    import numpy as np
     from sksound.sounds import Sound
     import matplotlib.pyplot as plt
 except ImportError as e:
-    installs = ['docopt', 'scikit-sound', 'matplotlib']
+    installs = ['opencv-python', 'docopt', 'numpy', 'scikit-sound', 'matplotlib']
     sys.stderr.write('Error: %s\nTry:\n    pip install --user %s\n' % (e, ' '.join(installs)))
     sys.exit(1)
 
 
-def gen_wave_plot(input_file, output_file, height, width, channels):
+def generate_waveform_image(input_file, height, width, channels):
+    ''' Generate an waveform image, two dimensional representation of the input audio '''
     mysound = Sound(input_file)
 
     # print(repr(mysound.data.view()))
@@ -37,7 +42,7 @@ def gen_wave_plot(input_file, output_file, height, width, channels):
     x = mysound.data
 
     dpi = 20
-    fig = plt.figure(dpi=dpi, figsize=(width / dpi, height / dpi))
+    # fig = plt.figure(dpi=dpi, figsize=(width / dpi, height / dpi))
 
     if channels == 2:
         plt.subplot(2, 1, 1)
@@ -58,7 +63,33 @@ def gen_wave_plot(input_file, output_file, height, width, channels):
         plt.plot(x[:, 0])
 
     # plt.show()
-    plt.savefig(output_file)
+    return plt
+
+
+def generate_waveform_video(background_image, total_seconds, output_file, fps):
+    image = cv2.imread(background_image)
+    height, width, _ = image.shape
+
+    frames = int(total_seconds * fps)
+    color = (40, 40, 255)
+    thickness = 2
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video = cv2.VideoWriter(output_file, fourcc, float(fps), (width, height))
+
+    for frame in range(frames):
+        update_period = 1000
+        if frame % update_period == 0:
+            print('%3.2f %d %d' % (frame / 24, frame, frames))
+
+        paint_x = int(frame / frames * width)
+
+        frame = copy.copy(image)
+
+        cv2.line(frame, (paint_x, 0), (paint_x, height), color, thickness)
+        video.write(frame)
+
+    video.release()
 
 
 def main(args):
@@ -66,15 +97,22 @@ def main(args):
     input_file = args['<input_media_file>']
     output_file = args['--output']
     try:
+        total_seconds = float(args['<total_seconds>'])
         height = int(args['--height'])
         width = int(args['--width'])
         channels = int(args['--channels'])
+        fps = int(args['--fps'])
     except ValueError as e:
         print('Error: %s\n' % str(e))
         print(__doc__)
         return 2
 
-    gen_wave_plot(input_file, output_file, height, width, channels)
+    background_filename = output_file + '.background.png'
+
+    im_background = generate_waveform_image(input_file, height, width, channels)
+    im_background.savefig(background_filename)
+
+    generate_waveform_video(background_filename, total_seconds, output_file, fps)
 
     return 0
 
